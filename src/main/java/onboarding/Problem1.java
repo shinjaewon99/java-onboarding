7da0b20f0185aa4a49dcbcbc6da88193ea61a832
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-20 00:05:28 +0900

    refactor : problem1 2일차 연습 리펙터링

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -19,7 +19,7 @@ class Problem1 {
         int rightPage = inputList.get(1);
         int sum = 0;
         while (rightPage != 0) {
-            sum += inputList.get(1) % 10;
+            sum += rightPage % 10;
             rightPage/=10;
         }
         return sum;
@@ -54,18 +54,54 @@ class Problem1 {
     }
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int leftPobiSum = sumLeft(pobi);
-        int leftCrongSum = sumLeft(crong);
-        int leftSumMax = Math.max(leftPobiSum, leftCrongSum);
 
-        int leftPobiMulti = multiLeft(pobi);
-        int leftCrongMulti = multiLeft(crong);
-        int leftMultiMax = Math.max(leftPobiMulti, leftCrongMulti);
+        int leftPobiMax = Math.max(sumLeft(pobi), multiLeft(pobi));
+        int rightPobiMax = Math.max(sumRight(pobi), multiRight(pobi) );
 
-        int totalLeftMax = Math.max(leftSumMax, leftMultiMax);
 
-        return leftPobiSum;
+        int totalpobi = Math.max(leftPobiMax, rightPobiMax);
 
+
+
+        int leftCrongMax = Math.max(sumLeft(crong), multiLeft(crong));
+        int rightCrongMax = Math.max(sumRight(crong), multiRight(crong));
+
+        int totalcrong = Math.max(leftCrongMax, rightCrongMax);
+
+        if(!validate(pobi) || !validate(crong)){
+            return -1;
+        }
+
+        return findWinner(totalpobi, totalcrong);
+    }
+
+    private static boolean validate(List<Integer> inputPages) {
+
+        // 페이지가 순서대로 입력되었는지 검증
+        if(inputPages.get(1) - inputPages.get(0) != 1){
+            return false;
+        }
+        // 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수 인지 검증
+        if(inputPages.get(0) % 2 == 0 || inputPages.get(1) % 2 !=0){
+            return false;
+        }
+
+        if(inputPages.size() !=2){
+            return false;
+        }
+        return true;
+    }
+
+    private static int findWinner(int pobiMax, int crongMax) {
+        if (pobiMax > crongMax) {
+            return 1;
+        }
+
+        if (pobiMax < crongMax) {
+            return 2;
+        }
+        return 0;
 
     }
+
 }
